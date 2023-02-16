612a98d3b7fdb13e2b02b76e34f9a903402fcb60
sophia0107 <125468828+sophia0107@users.noreply.github.com>
2023-02-16 11:17:49 +0800

    Add files via upload

---
+++ HW6/mc9727_hw6_q3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+double eApprox(int n);
+
+int main() {
+    cout.precision(30);
+
+    for(int n = 1; n <= 15; n++) {
+        cout << "n = " << n << '\t' << eApprox(n) << endl;
+    }
+    return 0;
+}
+
+double eApprox(int n) {
+    int i;
+    double e = 1.0;
+    double factorial = 1.0;
+    
+    for(i = 1; i <= n; i++) {
+        factorial *= i;
+        e += 1.0/factorial;
+    }
+    return e;
+}
