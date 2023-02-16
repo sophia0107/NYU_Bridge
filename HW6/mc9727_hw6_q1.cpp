dfbf5c88463c99c4d3754702156ff4722ad47216
sophia0107 <125468828+sophia0107@users.noreply.github.com>
2023-02-16 10:27:27 +0800

    Update and rename q1.cpp to mc9727_hw6_q1.cpp

--- HW6/mc9727_hw6_q1.cpp
+++ HW6/mc9727_hw6_q1.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int fib(int n);
-int main()
-{
+
+int main() {
     int num, fn;
     cout << "Please enter a positive integer: ";
     cin >> num;
@@ -12,14 +12,11 @@ int main()
     return 0;
 }
 
-int fib(int n)
-{
-    if(n <= 2)
-    {
+int fib(int n) {
+    if(n <= 2) {
         return 1;
     }
-    else
-    {
+    else {
         return fib(n - 1) + fib(n - 2);
     }
 }
